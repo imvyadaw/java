8f7e2dcbc2567863bc8de54e9c0b43231641b9a3
imvyadaw <vishalyadav8466@gmail.com>
2025-02-14 23:48:31 +0530

    update

--- Arrays/Arrays.java
+++ Arrays/Arrays.java
@@ -9,12 +9,9 @@ public class Arrays {
         arr[3]=400;
         arr[4]=500;
         arr[5]=600;
-        System.out.println(arr[0]);
-        System.out.println(arr[1]);
-        System.out.println(arr[2]);
-        System.out.println(arr[3]);
-        System.out.println(arr[4]);
-        System.out.println(arr[5]);
+        // System.out.println(arr[0]);
+        // System.out.println(arr[2]);
+
 
     }
     
---
+++ Arrays/for_arraay.java
@@ -0,0 +1,14 @@
+package Arrays;
+
+public class for_arraay {
+    public static void main(String[] args) {
+        int [] New ={10,20,30,40,80};
+        // System.out.println(New[0]);
+        // System.out.println(New.length);
+
+        for(int i=0;i<New.length;i=i+1){
+            System.out.println(New[i]);
+        }
+    }
+    
+}
